3ec1e1b595635efbfbf0e7f247e2a86bf541b453
GioMarcolla <gmarcolla@outlook.com>
2025-06-17 18:03:11 +0200

    Fixing attention mistake. Wrong schema.

--- src/routes/experienceDataRoutes.ts
+++ src/routes/experienceDataRoutes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from "fastify";
 import { EducationDataSchema } from "../schemas/educationDataSchema.js";
 import { ExperienceDataType } from "../types/experienceDataType.js";
 import { ExperienceData } from "../data/experienceData.js";
+import { ExperienceDataSchema } from "../schemas/experienceDataSchema.js";
 
 export const ExperienceDataRoutes = async (fastify: FastifyInstance) => {
     fastify.get(
         "/experienceData",
-        { schema: EducationDataSchema },
+        { schema: ExperienceDataSchema },
         async (): Promise<ExperienceDataType[]> => {
             return ExperienceData;
         }
