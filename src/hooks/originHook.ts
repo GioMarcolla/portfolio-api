30061212df179c5aaeaebcdd0edfb0b1b8678392
GioMarcolla <gmarcolla@outlook.com>
2026-01-17 01:09:43 +0800

    Removed redundant origin check:

--- src/hooks/originHook.ts
+++ src/hooks/originHook.ts
@@ -12,19 +12,20 @@ export async function verifyOrigin(
     if (request.method === "OPTIONS") return;
     if (/^\/public(\/[^\/]+)+/.test(request.url || "")) return;
 
-    const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",");
-    if (isDev) allowedOrigins.push("http://localhost:3000")
+    // const allowedOrigins = (process.env.ALLOWED_ORIGINS || "").split(",");
+    // if (isDev) allowedOrigins.push("http://localhost:3000")
 
-    const origin = request.headers["origin"] || request.headers["referer"];
-    if (
-        !allowedOrigins ||
-        !origin ||
-        !allowedOrigins.some((o) => origin.startsWith(o))
-    ) {
-        if (!allowedOrigins)
-            logger.error(
-                "Missing Allowed Origins. All routes will be blocked. Please update the enviroment variables."
-            );
-        return reply.status(401).send({ error: "Forbidden" });
-    }
+    // const origin = request.headers["origin"] || request.headers["referer"];
+    // if (
+    //     !allowedOrigins ||
+    //     !origin ||
+    //     !allowedOrigins.some((o) => origin.startsWith(o))
+    // ) {
+    //     if (!allowedOrigins)
+    //         logger.error(
+    //             "Missing Allowed Origins. All routes will be blocked. Please update the enviroment variables."
+    //         );
+    //     return reply.status(401).send({ error: "Forbidden" });
+    // }
+    return 
 }
